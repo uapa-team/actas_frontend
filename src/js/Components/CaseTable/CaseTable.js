77bc0dd9b48ff0c0384bba8076bbe25de9f3be63
Fortich <ajsuarezf@unal.edu.co>
2020-02-05 09:47:43 -0500

    Fixed last commit, added packege-lock.json

--- src/js/Components/CaseTable/CaseTable.js
+++ src/js/Components/CaseTable/CaseTable.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Table } from 'antd';
+import { Table, Divider } from 'antd';
 import auth from "../../../auth"
 import { withRouter } from "react-router-dom"
 import BackEndUrl from '../../../backendurl'
@@ -13,17 +13,26 @@ class CaseTable extends React.Component {
   } 
   render() {
     var columns = [
-      { title: 'Código',dataIndex: 'id',key: 'id',},
       { title: 'Tipo', dataIndex: '_cls_display', key: '_cls_display', },
       { title: 'DNI', dataIndex: 'student_dni', key: 'student_dni', },
+      { title: 'Nombres', dataIndex: 'student_name', key: 'student_name', },
       { title: 'Plan', dataIndex: 'academic_program', key: 'academic_program', },
-      { title: 'Creación', dataIndex: 'date_stamp', key: 'date_stamp', },
-      { title: 'Radicación', dataIndex: 'date', key: 'date', },
+      { title: 'Creación', dataIndex: 'date_stamp', key: 'date_stamp',  width: '10%'},
+      { title: 'Radicación', dataIndex: 'date', key: 'date',  width: '10%'},
       { title: 'Número', dataIndex: 'consecutive_minute', key: 'consecutive_minute', },
       { title: 'Año', dataIndex: 'year', key: 'year', },
-      { title: 'Periodo', dataIndex: 'academic_period', key: 'academic_period', },
-      { title: 'Rta CF', dataIndex: 'approval_status', key: 'approval_status', },
-      { title: 'Rta CA', dataIndex: 'advisor_response', key: 'advisor_response', },]
+      { title: 'Periodo', dataIndex: 'academic_period', key: 'academic_period',  width: '8%'},
+      { title: 'Rta CF', dataIndex: 'approval_status', key: 'approval_status',  width: '8%'},
+      { title: 'Rta CA', dataIndex: 'advisor_response', key: 'advisor_response', width: '8%' },
+      {
+        title: 'Editar', key: 'edit', width: '8%', render: (text, record) => (
+          <span>
+            <a>Editar</a>
+            <Divider type="vertical" />
+            <a>Anular</a>
+          </span>
+        )}
+    ]
     return (
       <Table dataSource={this.state.dataSource} columns={columns} />
     );
@@ -37,7 +46,7 @@ class CaseTable extends React.Component {
         'Authorization': 'Token ' + auth.getToken(),
       }
     }).then(response => response.json())
-      .then(data => this.setState({dataSource: data['cases']}))
+      .then(data => this.setState({ dataSource: data['cases'] }))
   }
 }
 
