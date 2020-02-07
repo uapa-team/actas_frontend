d04dd8c945fde141d9673bf8a5d70af0eea1f10f
Daniel Escobar <daescobarp@unal.edu.co>
2020-02-07 16:40:12 -0500

    ordered components

--- src/js/Components/Edit/MutableComponent.js
+++ src/js/Components/Edit/MutableComponent.js
@@ -8,7 +8,7 @@ import {
   Select,
   InputNumber
 } from "antd";
-import { Row } from "antd";
+import { Row, Col } from "antd";
 import { withRouter } from "react-router-dom";
 
 const { Text } = Typography;
@@ -48,99 +48,137 @@ class MutableComponent extends React.Component {
     if (this.props.metadata.type === "String") {
       if (Object.keys(this.props.metadata).includes("choices")) {
         return (
-          <Row>
-            <Text>{this.props.metadata.display}</Text>
-            <Form.Item>
-              <Select
-                showSearch
-                placeholder={this.props.metadata.display}
-                optionFilterProp="children"
-                filterOption={(input, option) =>
-                  option.props.children.toLowerCase().indexOf(
-                    input
-                      .toLowerCase()
-                      .normalize("NFD")
-                      .replace(/[\u0300-\u036f]/g, "")
-                  ) >= 0
-                }
-              >
-                {this.menuJS()}
-              </Select>
-            </Form.Item>
+          <Row type="flex" align="middle">
+            <Col span={12} style={{ textAlign: "right", paddingRight: "10px" }}>
+              <Text>{this.props.metadata.display}</Text>
+            </Col>
+            <Col span={12}>
+              <Form.Item style={{ marginBottom: "0px", paddingLeft: "10px" }}>
+                <Select
+                  showSearch
+                  placeholder={this.props.metadata.display}
+                  optionFilterProp="children"
+                  filterOption={(input, option) =>
+                    option.props.children.toLowerCase().indexOf(
+                      input
+                        .toLowerCase()
+                        .normalize("NFD")
+                        .replace(/[\u0300-\u036f]/g, "")
+                    ) >= 0
+                  }
+                >
+                  {this.menuJS()}
+                </Select>
+              </Form.Item>
+            </Col>
           </Row>
         );
       } else {
         return (
-          <Row>
-            <Text>{this.props.metadata.display}</Text>
-            <Form.Item>
-              <Input
-                placeholder={this.props.metadata.display}
-                defaultValue={this.props.metadata.default}
-              />
-            </Form.Item>
+          <Row type="flex" align="middle">
+            <Col span={12} style={{ textAlign: "right", paddingRight: "10px" }}>
+              <Text>{this.props.metadata.display}</Text>
+            </Col>
+            <Col span={12}>
+              <Form.Item style={{ marginBottom: "0px", paddingLeft: "10px" }}>
+                <Input
+                  placeholder={this.props.metadata.display}
+                  defaultValue={this.props.metadata.default}
+                />
+              </Form.Item>
+            </Col>
           </Row>
         );
       }
     } else if (this.props.metadata.type === "Integer") {
       return (
-        <Row>
-          <Text>{this.props.metadata.display}</Text>
-          <Form.Item>
-            <InputNumber defaultValue={this.props.metadata.default} />
-          </Form.Item>
+        <Row type="flex" align="middle">
+          <Col span={12} style={{ textAlign: "right", paddingRight: "10px" }}>
+            <Text>{this.props.metadata.display}</Text>
+          </Col>
+          <Col span={12}>
+            <Form.Item style={{ marginBottom: "0px", paddingLeft: "10px" }}>
+              <InputNumber defaultValue={this.props.metadata.default} />
+            </Form.Item>
+          </Col>
         </Row>
       );
     } else if (this.props.metadata.type === "Float") {
       return (
-        <Row>
-          <Text>{this.props.metadata.display}</Text>
-          <Form.Item>
-            <InputNumber
-              defaultValue={this.props.metadata.default}
-              step={0.1}
-            />
-          </Form.Item>
+        <Row type="flex" align="middle">
+          <Col span={12} style={{ textAlign: "right", paddingRight: "10px" }}>
+            <Text>{this.props.metadata.display}</Text>
+          </Col>
+          <Col span={12}>
+            <Form.Item style={{ marginBottom: "0px", paddingLeft: "10px" }}>
+              <InputNumber
+                defaultValue={this.props.metadata.default}
+                step={0.1}
+              />
+            </Form.Item>
+          </Col>
         </Row>
       );
     } else if (this.props.metadata.type === "Date") {
       return (
-        <Row>
-          <Text>{this.props.metadata.display}</Text>
-          <Form.Item>
-            {getFieldDecorator(this.props.fieldName)(
-              <DatePicker placeholder="Seleccione fecha" />
-            )}
-          </Form.Item>
+        <Row type="flex" align="middle">
+          <Col span={12} style={{ textAlign: "right", paddingRight: "10px" }}>
+            <Text>{this.props.metadata.display}</Text>
+          </Col>
+          <Col span={12}>
+            <Form.Item style={{ marginBottom: "0px", paddingLeft: "10px" }}>
+              {getFieldDecorator(this.props.fieldName)(
+                <DatePicker placeholder="Seleccione fecha" />
+              )}
+            </Form.Item>
+          </Col>
         </Row>
       );
     } else if (this.props.metadata.type === "Boolean") {
       return (
-        <Row>
-          <Text>{this.props.metadata.display}</Text>
-          <Form.Item>
-            {getFieldDecorator(this.props.fieldName, {
-              initialValue: this.props.metadata.default
-            })(<Checkbox />)}
-          </Form.Item>
+        <Row type="flex" align="middle">
+          <Col span={12} style={{ textAlign: "right", paddingRight: "10px" }}>
+            <Text>{this.props.metadata.display}</Text>
+          </Col>
+          <Col span={12}>
+            <Form.Item style={{ marginBottom: "0px", paddingLeft: "10px" }}>
+              {getFieldDecorator(this.props.fieldName)(
+                <Checkbox defaultChecked={this.props.metadata.default} />
+              )}
+            </Form.Item>
+          </Col>
         </Row>
       );
     } else if (this.props.metadata.type === "List:String") {
       return (
-        <Row>
-          <Text>{this.props.metadata.display}</Text>
-          <Select mode="tags" style={{ width: "100%" }} tokenSeparators={[","]}>
-            {children}
-          </Select>
+        <Row type="flex" align="middle">
+          <Col span={12} style={{ textAlign: "right", paddingRight: "10px" }}>
+            <Text>{this.props.metadata.display}</Text>
+          </Col>
+          <Col span={12}>
+            <Form.Item style={{ marginBottom: "0px", paddingLeft: "10px" }}>
+              <Select
+                mode="tags"
+                style={{ width: "100%" }}
+                tokenSeparators={[","]}
+              >
+                {children}
+              </Select>
+            </Form.Item>
+          </Col>
         </Row>
       );
     } else {
       return (
-        <Row>
-          <Text>{this.props.metadata.display}</Text>
-          <Form.Item>
-            <Input placeholder={this.props.fieldName} />
-          </Form.Item>
+        <Row type="flex" align="middle">
+          <Col span={12}>
+            <Text>{this.props.metadata.display}</Text>
+          </Col>
+          <Col span={12}>
+            <Form.Item style={{ marginBottom: "0px", paddingLeft: "10px" }}>
+              <Input placeholder={this.props.fieldName} />
+            </Form.Item>
+          </Col>
         </Row>
       );
     }
