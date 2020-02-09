77d5b4651e99adcd7bfe2b41899920a7f1bcd9ff
Daniel Escobar <daescobarp@unal.edu.co>
2020-02-09 01:29:12 -0500

    added preview working

--- src/js/Components/CaseTable/CaseTable.js
+++ src/js/Components/CaseTable/CaseTable.js
@@ -1,8 +1,33 @@
 import React from "react";
-import { Table } from "antd";
+import auth from "../../../auth";
+import BackEndUrl from "../../../backendurl";
+import { Table, Popconfirm, message } from "antd";
 import { withRouter } from "react-router-dom";
 
 class CaseTable extends React.Component {
+  confirmCancel = () => {
+    message.success("Solicitud anulada exitosamente");
+  };
+  generateCouncil = (isPre, recordId) => {
+    fetch(BackEndUrl + `generate?pre=${isPre}&id=${recordId}`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Token " + auth.getToken()
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        let link = BackEndUrl + data.url;
+        window.open(link, "_blank");
+      });
+    if (isPre) {
+      message.success("Acta de Comité Asesor Generada exitosamente");
+    } else {
+      message.success("Acta de Consejo de Facultad Generada exitosamente");
+    }
+  };
   render() {
     var columns = [
       { title: "Tipo", dataIndex: "_cls_display", key: "_cls_display" },
@@ -58,9 +83,28 @@ class CaseTable extends React.Component {
               Editar
             </a>
             <br />
-            <a href="delete">Anular</a>
+            {/* eslint-disable-next-line */}
+            <Popconfirm
+              title="¿Está seguro que desea anular la solicitud?"
+              onConfirm={this.confirmCancel}
+              okText="Sí"
+              cancelText="No"
+              placement="left"
+            >
+              <a>Anular</a>
+            </Popconfirm>
             <br />
-            <a href="preview">Vista previa</a>
+            {/* eslint-disable-next-line */}
+            <Popconfirm
+              title="¿Qué tipo de vista previa desea?"
+              onConfirm={() => this.generateCouncil(false, record.id)}
+              onCancel={() => this.generateCouncil(true, record.id)}
+              okText="Consejo"
+              cancelText="Comité"
+              placement="left"
+            >
+              <a>Vista Previa</a>
+            </Popconfirm>
           </span>
         )
       }
--- src/js/Components/Home/DrawerDownload.js
+++ src/js/Components/Home/DrawerDownload.js
@@ -2,40 +2,53 @@ import React from "react";
 import { withRouter } from "react-router-dom";
 import auth from "../../../auth";
 import BackEndUrl from "../../../backendurl";
-import {
-  Drawer,
-  Form,
-  Button,
-  Col,
-  Row,
-  Select,
-  Radio,
-  InputNumber
-} from "antd";
-const { Option } = Select;
+import { Drawer, Form, Button, Col, Row, Radio, InputNumber, Spin } from "antd";
+import "antd/dist/antd.css";
+import "./Home.css";
 class DrawerDownload extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      councilNumber: 0,
-      councliYear: 0,
+      councilNumber: 1,
+      councliYear: 2020,
       isPre: true,
       target: "",
-      allowed: []
+      allowed: [],
+      spinning: false
     };
   }
+  handleGenerate = () => {
+    this.setState({ spinning: true });
+    let query = `pre=${this.state.isPre}&consecutive_minute=${this.state.councliYear}&year=${this.state.councliYear}&${this.state.target}`;
+    fetch(BackEndUrl + `generate?${query}`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Token " + auth.getToken()
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ spinning: false });
+        let link = BackEndUrl + data.url;
+        window.open(link, "_blank");
+      });
+  };
   radioStyle = {
-    display: "block",
-    height: "30px",
-    lineHeight: "30px"
+    display: "flex",
+    whiteSpace: "normal"
   };
   menuJS = () => {
-    console.log(this.state.allowed);
     return this.state.allowed.map(this.radioBtn);
   };
   radioBtn = value => {
     return (
-      <Radio style={this.radioStyle} value={value[1].filter}>
+      <Radio
+        style={this.radioStyle}
+        value={value[1].filter}
+        key={value[1].filter}
+      >
         {value[1].display}
       </Radio>
     );
@@ -43,117 +56,124 @@ class DrawerDownload extends React.Component {
   render() {
     const { getFieldDecorator } = this.props.form;
     return (
-      <Drawer
-        title="Generar Acta del Consejo de Facultad o Comité Asesor"
-        width={420}
-        onClose={e => this.props.onClose(e, "Download")}
-        visible={this.props.visible}
-        bodyStyle={{ paddingBottom: 80 }}
-      >
-        <Form layout="vertical" hideRequiredMark>
-          <Row gutter={16}>
-            <Col span={12}>
-              <Form.Item label="Número del Acta">
-                {getFieldDecorator("number", {
-                  initialValue: 1,
-                  rules: [
-                    {
-                      required: true,
-                      message: "Por favor seleccione un número"
-                    }
-                  ]
-                })(
-                  <InputNumber
-                    placeholder="Número de acta"
-                    onChange={value => this.setState({ councilNumber: value })}
-                    min={0}
-                  />
-                )}
-              </Form.Item>
-            </Col>
-          </Row>
-          <Row gutter={16}>
-            <Col span={12}>
-              <Form.Item label="Año">
-                {getFieldDecorator("year", {
-                  initialValue: 2020,
-                  rules: [
-                    {
-                      required: true,
-                      message: "Por favor seleccione un año"
-                    }
-                  ]
-                })(
-                  <InputNumber
-                    placeholder="Número de acta"
-                    onChange={value => this.setState({ councliYear: value })}
-                    min={2000}
-                  />
-                )}
-              </Form.Item>
-            </Col>
-          </Row>
-          <Row gutter={16}>
-            <Col span={24}>
-              <Form.Item label="Tipo de Acta">
-                {getFieldDecorator("owner", {
-                  rules: [{ required: true, message: "Seleccione un valor" }],
-                  initialValue: true
-                })(
-                  <Radio.Group
-                    onChange={v => this.setState({ isPre: v.target.value })}
-                  >
-                    <Radio.Button value={false}>
-                      Consejo de Facultad
-                    </Radio.Button>
-                    <Radio.Button value={true}>Comité Asesor</Radio.Button>
-                  </Radio.Group>
-                )}
-              </Form.Item>
-            </Col>
-          </Row>
-          <Row gutter={16}>
-            <Col span={24}>
-              <Form.Item label="Solicitudes a generar">
-                {getFieldDecorator("generate", {
-                  rules: [{ required: true, message: "Seleccione una opción" }]
-                })(
-                  <Radio.Group
-                    onChange={v => this.setState({ target: v.target.value })}
-                  >
-                    {this.menuJS()}
-                  </Radio.Group>
-                )}
-              </Form.Item>
-            </Col>
-          </Row>
-        </Form>
-        <div
-          style={{
-            position: "absolute",
-            right: 0,
-            bottom: 0,
-            width: "100%",
-            borderTop: "1px solid #e9e9e9",
-            padding: "10px 16px",
-            background: "#fff",
-            textAlign: "right"
-          }}
+      <Spin spinning={this.state.spinning}>
+        <Drawer
+          title="Generar Acta del Consejo de Facultad o Comité Asesor"
+          width={420}
+          onClose={e => this.props.onClose(e, "Download")}
+          visible={this.props.visible}
+          bodyStyle={{ paddingBottom: 80 }}
         >
-          <Button
-            onClick={e => this.props.onClose(e, "Download")}
-            style={{ marginRight: 8 }}
+          <Form layout="vertical" hideRequiredMark>
+            <Row gutter={16}>
+              <Col span={12}>
+                <Form.Item label="Número del Acta">
+                  {getFieldDecorator("number", {
+                    initialValue: this.state.councilNumber,
+                    rules: [
+                      {
+                        required: true,
+                        message: "Por favor seleccione un número"
+                      }
+                    ]
+                  })(
+                    <InputNumber
+                      placeholder="Número de acta"
+                      onChange={value =>
+                        this.setState({ councilNumber: value })
+                      }
+                      min={0}
+                    />
+                  )}
+                </Form.Item>
+              </Col>
+            </Row>
+            <Row gutter={16}>
+              <Col span={12}>
+                <Form.Item label="Año">
+                  {getFieldDecorator("year", {
+                    initialValue: this.state.councliYear,
+                    rules: [
+                      {
+                        required: true,
+                        message: "Por favor seleccione un año"
+                      }
+                    ]
+                  })(
+                    <InputNumber
+                      placeholder="Número de acta"
+                      onChange={value => this.setState({ councliYear: value })}
+                      min={2000}
+                    />
+                  )}
+                </Form.Item>
+              </Col>
+            </Row>
+            <Row gutter={16}>
+              <Col span={24}>
+                <Form.Item label="Tipo de Acta">
+                  {getFieldDecorator("owner", {
+                    rules: [{ required: true, message: "Seleccione un valor" }],
+                    initialValue: this.state.isPre
+                  })(
+                    <Radio.Group
+                      onChange={v => this.setState({ isPre: v.target.value })}
+                    >
+                      <Radio.Button value={false}>
+                        Consejo de Facultad
+                      </Radio.Button>
+                      <Radio.Button value={true}>Comité Asesor</Radio.Button>
+                    </Radio.Group>
+                  )}
+                </Form.Item>
+              </Col>
+            </Row>
+            <Row gutter={16}>
+              <Col span={24}>
+                <Form.Item label="Solicitudes a generar">
+                  {getFieldDecorator("generate", {
+                    initialValue: this.state.target
+                  })(
+                    <Radio.Group
+                      onChange={v => this.setState({ target: v.target.value })}
+                    >
+                      {this.menuJS()}
+                    </Radio.Group>
+                  )}
+                </Form.Item>
+              </Col>
+            </Row>
+          </Form>
+          <div
+            style={{
+              position: "absolute",
+              right: 0,
+              bottom: 0,
+              width: "100%",
+              borderTop: "1px solid #e9e9e9",
+              padding: "10px 16px",
+              background: "#fff",
+              textAlign: "right"
+            }}
           >
-            Cancel
-          </Button>
-          <Button
-            onClick={e => this.props.onClose(e, "Download")}
-            type="primary"
-          >
-            Generar
-          </Button>
-        </div>
-      </Drawer>
+            <Button
+              onClick={e => this.props.onClose(e, "Download")}
+              style={{ marginRight: 8 }}
+            >
+              Cancel
+            </Button>
+            <Button
+              onClick={e => {
+                this.handleGenerate();
+                this.props.onClose(e, "Download");
+              }}
+              type="primary"
+            >
+              Generar
+            </Button>
+          </div>
+        </Drawer>
+      </Spin>
     );
   }
   componentDidMount() {
